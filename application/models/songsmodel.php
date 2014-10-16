b952ffc4a272457986ec8df47b5f9488eb88ef2a
Jeffrey <jefferson_dnb@hotmail.com>
2014-10-16 15:51:18 -0300

    Alterações

--- application/models/songsmodel.php
+++ application/models/songsmodel.php
@@ -1,13 +1,12 @@
 <?php
 
-class SongsModel
-{
+class SongsModel {
+
     /**
      * Every model needs a database connection, passed to the model
      * @param object $db A PDO database connection
      */
-    function __construct($db)
-    {
+    function __construct($db) {
         try {
             $this->db = $db;
         } catch (PDOException $e) {
@@ -18,8 +17,7 @@ class SongsModel
     /**
      * Get all songs from database
      */
-    public function getAllSongs()
-    {
+    public function getAllSongs() {
         $sql = "SELECT id, artist, track, link FROM song";
         $query = $this->db->prepare($sql);
         $query->execute();
@@ -37,8 +35,7 @@ class SongsModel
      * @param string $track Track
      * @param string $link Link
      */
-    public function addSong($artist, $track, $link)
-    {
+    public function addSong($artist, $track, $link) {
         // clean the input from javascript code for example
         $artist = strip_tags($artist);
         $track = strip_tags($track);
@@ -55,10 +52,27 @@ class SongsModel
      * add/update/delete stuff!
      * @param int $song_id Id of song
      */
-    public function deleteSong($song_id)
-    {
+    public function deleteSong($song_id) {
         $sql = "DELETE FROM song WHERE id = :song_id";
         $query = $this->db->prepare($sql);
         $query->execute(array(':song_id' => $song_id));
     }
+
+    public function getSong($song_id) {
+        $sql = "SELECT id, artist, track, link FROM song WHERE id = :song_id LIMIT 1";
+        $query = $this->db->prepare($sql);
+        $query->execute(array(':song_id' => $song_id));
+        return $query->fetch();
+    }
+      public function updateSong($song_id, $artist, $track, $link) {
+        // clean the input from javascript code for example
+        $artist = strip_tags($artist);
+        $track = strip_tags($track);
+        $link = strip_tags($link);
+
+        $sql = "UPDATE song SET artist = :artist, track = :track, link = :link WHERE id = :song_id";
+        $query = $this->db->prepare($sql);
+        $query->execute(array(':artist' => $artist, ':track' => $track, ':link' => $link, ':song_id' => $song_id));
+    }
+
 }
